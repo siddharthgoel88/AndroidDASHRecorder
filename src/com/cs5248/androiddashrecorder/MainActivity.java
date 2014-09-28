b05b7b2b5477efa65db4368346f5624570e9534a
Siddharth Goel <siddharth98391@gmail.com>
2014-09-28 20:31:57 +0800

    Added required permission and video code
    Signed-off-by: Siddharth Goel <siddharth98391@gmail.com>

--- src/com/cs5248/androiddashrecorder/MainActivity.java
+++ src/com/cs5248/androiddashrecorder/MainActivity.java
@@ -1,15 +1,33 @@
 package com.cs5248.androiddashrecorder;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	
+
+    static final int REQUEST_VIDEO_CAPTURE = 1;
+    private Intent videoIntent;
+
+	private void dispatchTakeVideoIntent() {
+	    videoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+	    if (videoIntent.resolveActivity(getPackageManager()) != null) {
+	        startActivityForResult(videoIntent, REQUEST_VIDEO_CAPTURE);
+	    }
+}	
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //TextView tv = (TextView) findViewById(R.id.helloId);
+        //tv.setText("Yuhu !!!");
+        dispatchTakeVideoIntent();
     }
 }
