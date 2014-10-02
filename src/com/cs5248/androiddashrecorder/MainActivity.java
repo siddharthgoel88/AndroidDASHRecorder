3f937738b6c7751df7c3080fdee092d41f448148
Siddharth Goel <siddharth98391@gmail.com>
2014-10-02 20:56:09 +0800

    Setting location to recorded video
    Logic still incomplete. Need to complete it.
    
    Signed-off-by: Siddharth Goel <siddharth98391@gmail.com>

--- src/com/cs5248/androiddashrecorder/MainActivity.java
+++ src/com/cs5248/androiddashrecorder/MainActivity.java
@@ -1,9 +1,16 @@
 package com.cs5248.androiddashrecorder;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,15 +21,35 @@ public class MainActivity extends Activity {
 
     static final int REQUEST_VIDEO_CAPTURE = 1;
     private Intent videoIntent;
+    private static final String APP_ROOT_DIR = "";
+    private Uri fileUri;
 
 	private void dispatchTakeVideoIntent() {
 	    videoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+	    //fileUri = getOutputMediaFileUri(); // create a file to save the video
+	    //videoIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+	    
 	    if (videoIntent.resolveActivity(getPackageManager()) != null) {
 	        startActivityForResult(videoIntent, REQUEST_VIDEO_CAPTURE);
 	    }
+	    //Log.i("MainActivity:dispatchTakeVideoIntent" ,videoIntent.getData().toString());
 }	
 
-    @Override
+    /**
+     * Convenience method to generate the path where to store the video 
+     * recording.
+     * @return Uri where the recording is to be saved
+     */
+	private Uri getOutputMediaFileUri() {
+		String filename = new SimpleDateFormat("dd_MM_yyyy_hh_mm").format(new Date()) + ".mp4";
+		File imagesFolder = new File(Environment.getExternalStorageDirectory(), APP_ROOT_DIR);
+		imagesFolder.mkdirs();
+		File image = new File(imagesFolder, filename);
+		Uri uriSavedImage = Uri.fromFile(image);
+		return uriSavedImage;
+	}
+
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -35,6 +62,7 @@ public class MainActivity extends Activity {
 				dispatchTakeVideoIntent();
 			}
 		});
+        
     }
     
     
