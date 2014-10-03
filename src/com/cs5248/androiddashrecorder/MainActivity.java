a4b9384a65453fa9964a91b28b62e02ab6ff21f7
Siddharth Goel <siddharth98391@gmail.com>
2014-10-03 19:02:17 +0800

    File name and directory structure for video file
    Added a specific directory to store the video recorded. The filename
    will contain the timestamp of its record start time.
    
    Signed-off-by: Siddharth Goel <siddharth98391@gmail.com>

--- src/com/cs5248/androiddashrecorder/MainActivity.java
+++ src/com/cs5248/androiddashrecorder/MainActivity.java
@@ -16,23 +16,22 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+
 public class MainActivity extends Activity {
 	
 
     static final int REQUEST_VIDEO_CAPTURE = 1;
     private Intent videoIntent;
-    private static final String APP_ROOT_DIR = "";
+    private static final String APP_ROOT_DIR = "DASHRecorder";
     private Uri fileUri;
 
 	private void dispatchTakeVideoIntent() {
 	    videoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
-	    //fileUri = getOutputMediaFileUri(); // create a file to save the video
-	    //videoIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
-	    
+	    fileUri = getOutputMediaFileUri(); // create a file to save the video
+	    videoIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
 	    if (videoIntent.resolveActivity(getPackageManager()) != null) {
 	        startActivityForResult(videoIntent, REQUEST_VIDEO_CAPTURE);
 	    }
-	    //Log.i("MainActivity:dispatchTakeVideoIntent" ,videoIntent.getData().toString());
 }	
 
     /**
@@ -41,10 +40,16 @@ public class MainActivity extends Activity {
      * @return Uri where the recording is to be saved
      */
 	private Uri getOutputMediaFileUri() {
-		String filename = new SimpleDateFormat("dd_MM_yyyy_hh_mm").format(new Date()) + ".mp4";
-		File imagesFolder = new File(Environment.getExternalStorageDirectory(), APP_ROOT_DIR);
-		imagesFolder.mkdirs();
-		File image = new File(imagesFolder, filename);
+		String foldername = APP_ROOT_DIR + "/video" ;
+		String filename = new SimpleDateFormat("dd_MM_yyyy_hh_mm_ss").format(new Date()) + ".mp4";
+		File videoFolder = new File(Environment.getExternalStorageDirectory(), foldername);
+		videoFolder.mkdirs();
+
+		// Delete all previous files in video folder
+		for (File tmp : videoFolder.listFiles())
+			  tmp.delete();
+
+		File image = new File(videoFolder, "DASH_Video_" + filename);
 		Uri uriSavedImage = Uri.fromFile(image);
 		return uriSavedImage;
 	}
